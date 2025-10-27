ac039003c0fa93bf6000b919c1d19cc3323380cc
Souravroy99 <souravroy200418@gmail.com>
2025-10-27 15:02:56 +0530

    Working on sqlite

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de8774fb0b203997ccf06eb4ebd83f7c773bcf82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931cf724e024ccb1958cf91633512cbc45d5ca99")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
